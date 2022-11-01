13ab0692b035a79bd213480630eb06049bd43b31
Owen Lee <47197696+KermityOwen@users.noreply.github.com>
2022-11-02 02:30:12 +0800

    another one

--- src/CardGame.java
+++ src/CardGame.java
@@ -1,24 +1,54 @@
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class CardGame {
 
+    // Maps for players and decks
     HashMap<Integer, Player> playersMap;
     HashMap<Integer, CardDeck> deckMap;
 
+    // Number of players + numbers of decks (used for circular calculations with modulo later)
+    int nPlayersDecks;
+
+    synchronized static int[] parseDeckFile(String filePath){
+        // TBD
+        return null;
+    }
+
+    synchronized static String inputString(String prompt){
+        Scanner s = new Scanner(System.in);
+        System.out.println(prompt);
+        return s.nextLine();
+    }
+
     public CardGame(int nOfPlayers, int[] cvs) throws IncorrectCardsInPackException {
+        // Guards
         if (cvs.length != 8*nOfPlayers){
-            throw new IncorrectCardsInPackException("Error");
+            throw new IncorrectCardsInPackException("Pack doesn't have 8*(N of Players) cards");
         }
 
+        nPlayersDecks = nOfPlayers *2;
         int[][] cardSplits = Utilities.splitIntArray(cvs, nOfPlayers*2); // INCOMPLETE
-        for (int i = 0; i < cardSplits.length-1;){
 
+        for (int i = 0; i < (nOfPlayers*2);){
+            playersMap.put(i, new Player(cardSplits[i], i));
+            deckMap.put(i+1, new CardDeck(cardSplits[i+1], i+1));
+            i = i+2;
         }
     }
 
-    public static void main(String[] args) throws IncorrectCardsInPackException {
+    public static void main(String[] args) {
         int nPlayers = Utilities.inputInt("Please enter the number of players: ");
-        CardGame mainGame = new CardGame(nPlayers, new int[]{1, 1, 1, 1});
+
+        // \/ need to edit after parseDeckFile adn inputString is relocated to the Utility class
+        int[] cardPack = parseDeckFile(inputString("Please enter location of the pack to load: "));
+
+        try {
+            CardGame mainGame = new CardGame(nPlayers, cardPack);
+        } catch (IncorrectCardsInPackException e){
+            System.out.println(e);
+        }
+
 
     }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,15 +1,20 @@
 public class Player {
 
-    CardDeck cards;
+    Card[] cards;
     int playerID;
 
-    public Player(CardDeck c, int id){
-        this.cards = c;
+    public Player(int[] cvs, int id){
+        // For easier construction an array of int is used instead of array of cards
+        cards = new Card[cvs.length];
+        for (int i = 0; i < cvs.length;){
+            cards[i] = new Card(cvs[i]);
+            i++;
+        };
         this.playerID = id;
     }
 
     public Card[] getCards(){
-        return cards.getCards();
+        return cards;
     }
 
     public int getPlayerID(){
@@ -17,11 +22,11 @@ public class Player {
     }
 
     public boolean validate(){
-        Card firstCard = cards.getCard(0);
+        Card firstCard = cards[0];
         // Start from 1st card, loop through the rest.
         // If any cards are different from the first, return false. If loop completes run without running into problems, return true
-        for (int i = 0; i < cards.getLength(); i++){
-            if (firstCard.getCardValue() != cards.getCard(i).getCardValue()) {
+        for (int i = 0; i < cards.length; i++){
+            if (firstCard.getCardValue() != cards[i].getCardValue()) {
                 // System.out.println("broken" + firstCard.getCardValue() + "//" + cards.getCard(i).getCardValue());
                 return false;
             }
